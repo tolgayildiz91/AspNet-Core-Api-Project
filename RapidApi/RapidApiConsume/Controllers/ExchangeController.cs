1ab6b094487ca32defa82ecd9c7f65a64e0a615f
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-08-19 22:34:59 +0300

    exchange api

---
+++ RapidApi/RapidApiConsume/Controllers/ExchangeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using RapidApiConsume.Models;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace RapidApiConsume.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<ExchangeViewModel> exchangeViewModels = new List<ExchangeViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=TRY&locale=en-gb"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "a04033069dmshbbb026391fcdfb4p15cbd7jsn20cec747e283" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values.exchange_rates.ToList());
+            }
+          
+        }
+    }
+}
