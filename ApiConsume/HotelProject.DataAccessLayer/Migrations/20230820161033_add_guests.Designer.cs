6fe366e43b48fa07749da5a2a90174acc054933e
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-08-20 19:51:38 +0300

    bugfix

--- ApiConsume/HotelProject.DataAccessLayer/Migrations/20230820161033_add_guests.Designer.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20230820161033_add_guests.Designer.cs
@@ -4,14 +4,16 @@ using HotelProject.DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HotelProject.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230820161033_add_guests")]
+    partial class add_guests
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
